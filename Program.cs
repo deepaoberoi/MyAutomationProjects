8d6a0cae1612ee65b0612cf69330dbfee96f4320
deepaoberoi <deepa.oberoi@centricconsulting.com>
2021-05-25 17:41:06 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumTests
+{
+    
+    public class Centric
+    {
+       
+       // private string baseURL;
+    
+    
+        
+         static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://centricconsultingllc.sharepoint.com/sites/TheLounge");
+            driver.FindElement(By.XPath("//*[@id=\"O365_AppName\"]/span")).Click();
+            driver.FindElement(By.XPath("//a[contains(.,'Celebrating Mental Health Awareness: Shedding the Stigma')]")).Click();
+        }
+       
+    }
+}
